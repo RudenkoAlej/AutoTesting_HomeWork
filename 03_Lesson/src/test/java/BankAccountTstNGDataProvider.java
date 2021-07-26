f2968b29a8d47abc637c3a05fea5b4b26a0570b8
Alexayurchuk <Alexarudenko@gmail.com>
2021-07-26 19:25:39 +0300

    BankAccount test coverage (Lesson 3) Fixed DataProvider from Test NG

--- 03_Lesson/src/test/java/BankAccountTstNGDataProvider.java
+++ 03_Lesson/src/test/java/BankAccountTstNGDataProvider.java
@@ -1,35 +1,51 @@
-//import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+
 
 import java.util.Arrays;
 
-//@RunWith(DataProviderRunner.class)
+@RunWith(DataProviderRunner.class)
 public class BankAccountTstNGDataProvider {
     private static BankAccount account = null;
-    private double number1;
-    private double result;
+    private float number1;
+    private float result;
 
-    @BeforeClass
+    @Before
     public void setUp() {
         account = new BankAccount(123123, 20.5f);
     }
 
-//    @DataProvider
+    @DataProvider
     public static Object[][] dataProviderIncreaseMoney() {
         return new Object[][] {
-                {20, 40.5},
-                {09.5, 30}};
+                {20f, 40.5f},
+                {09.5f, 30f}};
     }
 
     @Test
-//    @UseDataProvider("dataProviderIncreaseMoney")
-    public void checkIncreaseBalance() {
+    @UseDataProvider("dataProviderIncreaseMoney")
+    public void checkIncreaseBalance(float number1, float result) {
         account.credit(number1);
+        Assert.assertEquals(result, account.getBalance(), 0);
+    }
+
+    @DataProvider
+    public static Object[][] dataProviderDecreaseMoney() {
+        return new Object[][] {
+                {10f, 10.5f},
+                {15.5f, 5f}};
+    }
+
+    @Test
+    @UseDataProvider("dataProviderDecreaseMoney")
+    public void checkDecreaseBalance(float number1, float result) {
+        account.debit(number1);
         Assert.assertEquals(result, account.getBalance(), 0);
     }
 }
